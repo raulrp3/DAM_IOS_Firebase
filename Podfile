1535b6e287c9d181431133542bda07a9834fe4c3
raulrp3 <raul.ramirez@escuelaestech.es>
2020-01-13 16:42:42 +0100

    Añadidas dependencias de Firebase

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'DAM_IOS_Firebase' do
+
+	use_frameworks!
+	pod 'Firebase/Core'
+	pod 'Firebase/Auth'
+
+end
---
+++ Pods/Target Support Files/Pods-DAM_IOS_Firebase/Pods-DAM_IOS_Firebase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DAM_IOS_Firebase : NSObject
+@end
+@implementation PodsDummy_Pods_DAM_IOS_Firebase
+@end
